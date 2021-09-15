b17ac3ed8026ee064b7f17e9c7909f3eea64ef6a
Thiago <thiago.azevedo87@gmail.com>
2021-09-15 17:10:03 -0300

    cleaning, gitignore update

--- odometry/simulation_data_mpu6050.py
+++ odometry/simulation_data_mpu6050.py
@@ -6,6 +6,6 @@ file = "back"
 # file = "left"
 # file = "right"
 
-path = "/content/drive/MyDrive/dados_acelerometro/{}.csv".format(file)
+path = "../dados_acelerometro/{}.csv".format(file)
 data = pd.read_csv(path, sep=";", header=None)
 data = data.rename(columns={0: "Gx", 1: "Gy", 2: "Gz", 3: "Ax", 4: "Ay", 5: "Az"})
